7c694a53824fd996c207a6a11127f3a10fe66804
André Heie Vik <andrev@fidrildi.com>
2012-10-31 11:56:25 +0100

    fixed redis for local use

--- hello.rb
+++ hello.rb
@@ -3,8 +3,12 @@ require 'sinatra'
 
 configure do
   require 'redis'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  
+  if ENV["REDISTOGO_URL"]
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  end
+  
   begin
       REDIS.get("key")
   rescue
