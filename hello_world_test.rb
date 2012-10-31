f18ba3d0d8e23e37c4709448dfe0ea742751192e
André Heie Vik <andrev@fidrildi.com>
2012-10-31 13:00:47 +0100

    fix redis

---
+++ hello_world_test.rb
@@ -0,0 +1,20 @@
+require_relative 'hello'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.ok?
+    assert_equal 'Hello, world!', last_response.body
+  end
+
+end
